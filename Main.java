3a31a2a79d7175d0b8a3ce3eff601a7ba423429c
orencz <166212566+orencz@users.noreply.github.com>
2024-06-01 12:29:34 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import javafx.stage.DirectoryChooser;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -12,6 +13,7 @@ import java.io.File;
 public class Main extends Application {
     private TextField directoryPathField;
     private TextField searchField;
+    private TextArea resultArea; 
 
     public static void main(String[] args) {
         launch(args);
@@ -19,31 +21,34 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        
+       
         primaryStage.setTitle("File Browser and Search");
 
-        
         directoryPathField = new TextField();
         directoryPathField.setPromptText("Enter directory path");
 
+        
         searchField = new TextField();
         searchField.setPromptText("Enter search phrase");
 
-      
+       
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400); 
+
+     
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory(primaryStage));
 
-       
+        
         Button searchButton = new Button("Search");
 
-     
+        
         HBox hBox = new HBox(10, directoryPathField, browseButton);
 
-      
-        VBox vBox = new VBox(10, hBox, searchField, searchButton);
-        vBox.setPrefSize(600, 200);
+    
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
+        vBox.setPrefSize(600, 600); 
 
-        
         Scene scene = new Scene(vBox);
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -52,7 +57,7 @@ public class Main extends Application {
     private void browseDirectory(Stage primaryStage) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Select Directory");
-        
+
         File selectedDirectory = directoryChooser.showDialog(primaryStage);
 
         if (selectedDirectory != null) {
